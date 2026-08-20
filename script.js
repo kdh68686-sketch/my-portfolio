b597ee57479c87f1e1616668b9132638b00fc4c0
kdh68686-sketch <kdh68686@gmail.com>
2026-08-20 16:58:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// 🌙 다크 모드
 const themeButton = document.getElementById("themeButton");
 
 themeButton.addEventListener("click", function () {
     document.body.classList.toggle("dark");
 });
 
-
-// 📖 자세히 보기
 const detailButton = document.querySelector(".detailButton");
 const detailText = document.querySelector(".detailText");
 
