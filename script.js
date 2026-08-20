7a65488ddcea19c3783e74b4e40bd5030ff268eb
kdh68686-sketch <kdh68686@gmail.com>
2026-08-20 16:59:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// 다크 모드
 const themeButton = document.getElementById("themeButton");
 
 themeButton.addEventListener("click", function () {
     document.body.classList.toggle("dark");
 });
 
+
+// 자세히 보기
 const detailButton = document.querySelector(".detailButton");
 const detailText = document.querySelector(".detailText");
 
 detailButton.addEventListener("click", function () {
-    detailText.hidden = false;
-    detailButton.textContent = "접기";
+    if (detailText.hasAttribute("hidden")) {
+        detailText.removeAttribute("hidden");
+        detailButton.textContent = "접기";
+    } else {
+        detailText.setAttribute("hidden", "");
+        detailButton.textContent = "자세히 보기";
+    }
 });
