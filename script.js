c5e1ec01318f7c2644d426a46214a28c7a4facd9
kdh68686-sketch <kdh68686@gmail.com>
2026-08-20 16:50:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const button = document.getElementById("themeButton");
 button.addEventListener("click", function () {
     document.body.classList.toggle("dark");
 });
+
+
+const detailButton = document.querySelector(".detailButton");
+const detailText = document.querySelector(".detailText");
+
+detailButton.addEventListener("click", function () {
+    detailText.hidden = !detailText.hidden;
+
+    if (detailText.hidden) {
+        detailButton.textContent = "자세히 보기";
+    } else {
+        detailButton.textContent = "접기";
+    }
+});
